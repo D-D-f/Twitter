bb54f4b05fecc86be3083dd44b889b82e1082461
D-D-F <defreitas-david@outlook.fr>
2023-06-06 16:35:16 +0200

    update inscription

--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -1,6 +1,7 @@
 import classes from "./Inscription.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 import TextField from "@mui/material/TextField";
+import Button from "../../styledComponents/Button/Button";
 
 const Inscription = () => {
   return (
@@ -9,8 +10,22 @@ const Inscription = () => {
       <h2>Créer votre compte</h2>
 
       <TextField id="outlined-basic" label="Pseudo" variant="outlined" />
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <TextField id="outlined-basic" label="Password" variant="outlined" />
+      <TextField
+        style={{ marginTop: "15px" }}
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+      />
+      <TextField
+        style={{ marginTop: "15px", marginBottom: "15px" }}
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+      />
+
+      <Button className={classes.buttonInscription} type="submit">
+        {"S'inscrire"}
+      </Button>
     </form>
   );
 };
---
+++ src/styledComponents/Button/Button.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: var(--primaryColor);
+  color: white;
+  padding: 0.9rem 5rem;
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+export default Button;
