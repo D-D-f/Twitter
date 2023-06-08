70b405f1e0b5cebd532374f401e28d38579efac4
D-D-F <defreitas-david@outlook.fr>
2023-06-08 22:29:16 +0200

    update inscription

--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -6,7 +6,11 @@ import { useForm } from "react-hook-form";
 import { useState } from "react";
 
 const Inscription = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => console.log(data);
   const [hidden, setHidden] = useState(false);
 
@@ -25,25 +29,66 @@ const Inscription = () => {
       <h2>Créer votre compte</h2>
 
       <TextField
-        id="outlined-basic"
+        style={{ marginBottom: "5px" }}
         label="Pseudo"
-        {...register("Pseudo")}
+        {...register("Pseudo", {
+          required: "Le champ pseudo est requis.",
+          minLength: {
+            value: 4,
+            message: "Il faut un minimum de 4 caractères",
+          },
+        })}
         variant="outlined"
       />
+      <span
+        style={{
+          marginBottom: "15px",
+          color: "var(--colorError)",
+          fontWeight: "bold",
+        }}
+      >
+        {errors.Pseudo?.message}
+      </span>
       <TextField
-        style={{ marginTop: "15px" }}
-        id="outlined-basic"
+        style={{ marginBottom: "5px" }}
         label="Email"
-        {...register("Email")}
+        {...register("email", { required: "Le champ email est requis." })}
+        type="email"
+        autoComplete="username"
         variant="outlined"
       />
+      <span
+        style={{
+          marginBottom: "15px",
+          color: "var(--colorError)",
+          fontWeight: "bold",
+        }}
+      >
+        {errors.email?.message}
+      </span>
       <TextField
-        style={{ marginTop: "15px", marginBottom: "15px" }}
-        id="outlined-basic"
+        style={{ marginBottom: "5px" }}
         label="Password"
-        {...register("Password")}
+        {...register("password", {
+          required: "Le champ password est requis.",
+          minLength: {
+            value: 4,
+            message: "Il faut un minimum de 4 caractères",
+          },
+        })}
+        type="password"
+        autoComplete="current-password"
         variant="outlined"
       />
+      <span
+        style={{
+          marginBottom: "15px",
+          color: "var(--colorError)",
+          fontWeight: "bold",
+        }}
+      >
+        {errors.password?.message}
+      </span>
 
       <Button className={classes.buttonInscription} type="submit">
         {"S'inscrire"}
