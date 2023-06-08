39c40b9f44038cf75b5b8a46ae172fe1e8670a09
D-D-F <defreitas-david@outlook.fr>
2023-06-08 12:51:25 +0200

    get form

--- src/pages/Inscription/Inscription.jsx
+++ src/pages/Inscription/Inscription.jsx
@@ -2,24 +2,39 @@ import classes from "./Inscription.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 import TextField from "@mui/material/TextField";
 import Button from "../../styledComponents/Button/Button";
+import { useForm } from "react-hook-form";
 
 const Inscription = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <form className={classes.form_inscription} action="">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={classes.form_inscription}
+      action=""
+    >
       <CloseIcon className={classes.close} />
       <h2>Créer votre compte</h2>
 
-      <TextField id="outlined-basic" label="Pseudo" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="Pseudo"
+        {...register("Pseudo")}
+        variant="outlined"
+      />
       <TextField
         style={{ marginTop: "15px" }}
         id="outlined-basic"
         label="Email"
+        {...register("Email")}
         variant="outlined"
       />
       <TextField
         style={{ marginTop: "15px", marginBottom: "15px" }}
         id="outlined-basic"
         label="Password"
+        {...register("Password")}
         variant="outlined"
       />
 
